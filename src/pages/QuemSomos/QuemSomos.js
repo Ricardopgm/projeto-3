613e57870ca55ea7979877bfb41527c08ac1617c
Diegool97 <113364521+Diegool97@users.noreply.github.com>
2022-12-08 09:29:18 -0300

    Update QuemSomos.js

--- src/pages/QuemSomos/QuemSomos.js
+++ src/pages/QuemSomos/QuemSomos.js
@@ -3,12 +3,13 @@ import foto5 from '../../img/restaurante.jpg'
 
 const QuemSomos= () => {
     return <>
-<>
+
         <div className="text">
             <h1 className="quem">QuemSomos</h1>
             <h3>Fundado em 2022, o King Restaurant é gerenciado pelos proprietários Diego, Matheus, Milena, Ricardo e Suelen, criado pensando no melhor atendimento, 
               aliado com a melhor qualidade de produto, com profissionais qualificados na área da culinária. Nosso cardápio é diferenciado e único, trazendo diversos pratos executivos, sobremesas, petisco e drinks, perfeito para qualquer ocasião, fazendo com que nosso cliente se sinta à vontade em todos os seus momentos. Os pratos são bem servidos e toda essa excelência é apresentada a preços acessíveis. Nosso ambiente é amplo e muito aconchegante. Venha nos visitar! </h3>
        </div> 
+    
      
                          <img
                               className="restaurante"
