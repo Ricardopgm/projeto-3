dcdcb8997f9baab9107e38a3b3b410e521e4c51b
Suellen Siqueira <113906994+susueellen@users.noreply.github.com>
2022-12-07 21:53:56 -0300

    Pagina Loggin
    ..

--- src/pages/Loggin/Loggin.js
+++ src/pages/Loggin/Loggin.js
@@ -1,5 +1,54 @@
-const Loggin = () => {
-    return <h1>Loggin</h1>;
-  };
-  
-  export default Loggin;
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { useState } from 'react';
+import './styles.css';
+
+function Loggin() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  return (
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+
+            <span className="login-form-title"> Bem-vindo!</span>
+
+            <div className="wrap-input">
+              <input className={email !== "" ? 'has-val input' : 'input'}
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)} />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input className={password !== "" ? 'has-val input' : 'input'}
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)} />
+              <span className="focus-input" data-placeholder="Senha"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Não Possui Conta?</span>
+              <a className="txt2" href="#">Criar conta</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loggin;
+
+
